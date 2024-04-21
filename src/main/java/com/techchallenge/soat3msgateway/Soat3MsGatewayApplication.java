77c2ec72b120edeb65660efd3cfb31135f11aff7
VinniJr <vinni.jr@gmail.com>
2024-04-20 23:01:28 -0300

    Seu mensagem de commit aqui

---
+++ src/main/java/com/techchallenge/soat3msgateway/Soat3MsGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.techchallenge.soat3msgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Soat3MsGatewayApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soat3MsGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("soat3-ms-clientes", r -> r.path("/v1/clientes/**").uri("lb://soat3-ms-clientes"))
+				.build();
+	}
+
+}
