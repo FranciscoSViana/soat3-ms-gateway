69e7c1562513f6bf2b918e4b100f05062718f0e3
Francisco Viana <franciscosantos29@gmail.com>
2024-05-13 19:40:58 -0300

    criação manifestos yaml

--- src/main/java/com/techchallenge/soat3mseureka/Soat3MsGatewayApplication.java
+++ src/main/java/com/techchallenge/soat3mseureka/Soat3MsGatewayApplication.java
@@ -1,14 +1,14 @@
 package com.techchallenge.soat3mseureka;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class Soat3MsGatewayApplication {
 
 
@@ -16,14 +16,26 @@ public class Soat3MsGatewayApplication {
 		SpringApplication.run(Soat3MsGatewayApplication.class, args);
 	}
 
+	@Value("${CLIENTES_API_URL}")
+	private String clientesURL;
+
+	@Value("${PRODUTOS_API_URL}")
+	private String produtosURL;
+
+	@Value("${PAGAMENTOS_API_URL}")
+	private String pagamentosURL;
+
+	@Value("${PEDIDOS_API_URL}")
+	private String pedidosURL;
+
 
 	@Bean
 	public RouteLocator routes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route("soat3-ms-clientes", r -> r.path("/v1/clientes/**").uri("lb://soat3-ms-clientes"))
-				.route("soat3-ms-produtos", r -> r.path("/v1/produtos/**").uri("lb://soat3-ms-produtos"))
-				.route("soat3-ms-pagamentos", r -> r.path("/v1/pagamentos/**").uri("lb://soat3-ms-pagamentos"))
-				.route("soat3-ms-pedidos", r -> r.path("/v1/pedidos/**").uri("lb://soat3-ms-pedidos"))
+				.route("soat3-ms-clientes", r -> r.path("/v1/clientes/**").uri(clientesURL))
+				.route("soat3-ms-produtos", r -> r.path("/v1/produtos/**").uri(produtosURL))
+				.route("soat3-ms-pagamentos", r -> r.path("/v1/pagamentos/**").uri(pagamentosURL))
+				.route("soat3-ms-pedidos", r -> r.path("/v1/pedidos/**").uri(pedidosURL))
 				.build();
 	}
 
